94ebd80d86d96e8f5f26c9e1f241ffb99293c522
mariaclaratapicuru <lsmdantasmaria@gmail.com>
2025-06-27 09:31:23 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+st.title("Perguntados")
+st.text_input("Cadê o Josir?")
+st.text_input("Pêra ou maçã?)
+st.text_input("Flamengo é o maior do mundo: sim ou com certeza?")
+st.text_input("Qual seu sabor favorito de pizza?")
+st.button("Continuar")
